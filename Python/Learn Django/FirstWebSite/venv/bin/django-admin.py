c159cc10fde0229dbfb2510f64137fff30eb6a72
Thuận Nguyễn Văn <thuan2609.tk1@gmail.com>
2020-12-28 02:55:46 +0700

    Add New Project Python DjangoAnd Delete Empty Project Django

---
+++ Python/Learn Django/FirstWebSite/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nvt/Documents/Learn-Code/Python/Learn Django/FirstWebSite/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
